5045c0c280667478dc5bef1457adb2879344c0f9
Janak Sharma <janak.sharma2075@gmail.com>
2021-08-09 10:46:09 +0545

    ui form modification

--- DjangoBlog/Accounts/forms.py
+++ DjangoBlog/Accounts/forms.py
@@ -1,14 +1,52 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
+from django.forms import widgets
+from django.forms import ModelForm, TextInput, EmailInput
+
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100 )
-    last_name = forms.CharField(max_length=100 )
-    email = forms.EmailField(max_length=150)
+    first_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'style': 'max-width: 300px;',
+        'placeholder': 'FirstName'
+    }))
+    last_name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={
+        'class':'form-control',
+        'style': 'max-width: 300px;',
+        'placeholder': 'LastName'
+    }) )
+    email = forms.EmailField(max_length=100,widget=forms.EmailInput(attrs={
+        'class':'form-control',
+        'style': 'max-width: 300px;',
+        'placeholder': 'Email'
+    }))
+    password1=forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={
+        'class':'form-control',
+        'style': 'max-width: 300px;',
+        'placeholder': 'Password'
+    }))
+    password2=forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={
+        'class':'form-control',
+        'style': 'max-width: 300px;',
+        'placeholder': 'Confirm Password'
+    }))
 
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name',
 'email', 'password1', 'password2',)
+        widgets= {
+            'username': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Username'
+                }),
+              
+        }
+
+
+class LoginForm(AuthenticationForm):
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style': 'max-width: 300px;'}))
+    password =forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','style': 'max-width: 300px;'}))
